840696ffcf83c729f980b1790dca31847c2f1995
isra <isramartin157@gmail.com>
2025-05-26 13:01:24 -0600

    fix refrsh7

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,23 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
 export default defineConfig({
+  base: "/", // Para Render
   plugins: [react()],
   define: {
-    "process.env": process.env,
+    'process.env': process.env,
   },
   server: {
     open: true,
   },
-  base: "/", // <- CORREGIDO
   build: {
-    outDir: "dist",
+    outDir: 'dist',
     emptyOutDir: true,
     rollupOptions: {
       output: {
         assetFileNames: 'assets/[name]-[hash][extname]',
-        entryFileNames: 'assets/[name]-[hash].js'
-      }
-    }
+        entryFileNames: 'assets/[name]-[hash].js',
+      },
+    },
   },
 });
