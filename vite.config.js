3f50392467cc63f468deb3aba3f26f37ec960eb6
isra <isramartin157@gmail.com>
2025-05-26 12:02:06 -0600

    fix refrsh

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  
   plugins: [react()],
-   define: {
-    'process.env': process.env
+  define: {
+    "process.env": process.env,
   },
   server: {
-    open: true,  // Esto hará que se abra el navegador automáticamente
+    open: true, // Esto hará que se abra el navegador automáticamente
   },
-})
+  base: "/", // Asegúrate de que esté en '/' si tu app está en la raíz del dominio
+  build: {
+    outDir: "dist",
+    emptyOutDir: true, // Limpia el directorio de build previo
+  },
+});
